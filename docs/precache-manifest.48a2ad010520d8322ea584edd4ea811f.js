fbeb57a5938149f6aa852b85d79d5cc2620d8852
CarlosCE-dev <c4r10z.n3t@gmail.com>
2020-07-07 08:37:51 -0600

    Docs agregado

---
+++ docs/precache-manifest.48a2ad010520d8322ea584edd4ea811f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb61e2926bf20b7f48482492daaebbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebbc18ec728abaf76345",
+    "url": "/static/css/main.aa530b23.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbc18ec728abaf76345",
+    "url": "/static/js/main.5cb7e6e5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
